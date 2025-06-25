b04edb23f2a5e0bf862cbeb664dad3e86cd65fac
Leo Browaldh <leobrowaldh@gmail.com>
2025-06-25 15:54:37 +0200

    work in progress set on projects page

--- src/router/approuter.tsx
+++ src/router/approuter.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router";
 import Projects from '../pages/projects';
 import Cv from '../pages/cv';
+import Home from "../pages/home";
 
 
 
 export function AppRouter() {
     return (
         <Routes>
-            <Route path="/" element={<Cv/>} />
+            <Route path="/" element={<Home/>} />
             <Route path="/projects" element={<Projects/>} />
             <Route path="/cv" element={<Cv/>} />
         </Routes>
