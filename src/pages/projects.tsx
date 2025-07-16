4fddf44ef16f1cdeff3eb4c8d4946cef6574492a
Leo Browaldh <leobrowaldh@gmail.com>
2025-07-16 16:04:42 +0200

    changed project page, and added my two currently deployed projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,9 +1,10 @@
-import SharedLayoutAnimation from "../components/sharedLayoutAnimations";
+import ChromaGrid from "../components/chromaGrid";
 
 export default function Projects() {
     return (
-        <div className="justify-center items-center flex flex-col h-screen">
-            <SharedLayoutAnimation />
+        <div className="overflow-hidden h-screen">
+            <p className="m-10">Please be patient with the loading time and reolad the page on error (The applications are deployed on free tiers)</p>
+            <ChromaGrid />
         </div>
     )
 }
